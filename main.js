253f50d7832309e9b4b82b9f925e235f90495e61
website-egy <ranooda868@gmail.com>
2025-11-22 05:11:19 +0400

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-console.log("main.js loaded successfully!");
-document.addEventListener("DOMContentLoaded", function() {
-  const startQuizBtn = document.getElementById("startQuizBtn");
-  if (startQuizBtn) {
-    startQuizBtn.addEventListener("click", function() {
-      window.location.href = "quiz.html";
-    });
-  }
-});
